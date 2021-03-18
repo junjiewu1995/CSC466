bcb37a484b70e75a936b66cbf99fb770ef807ce2
justinbao <justinbao66@yahoo.ca>
2021-03-17 23:04:09 -0700

    Add server LoadCard() method

--- helper/RPC.go
+++ helper/RPC.go
@@ -10,6 +10,8 @@ type Args struct {
 }
 
 type Card struct {
+	/* Value Range: 2,3,4,5,6,7,8,9,10,J,Q,K,A */
+	/* Suit Range: clubs,diamonds,hearts,spades */
 	Value string
 	Suit string
 	Used bool
