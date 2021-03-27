3425ac97f6a91b8f9e7e45497abe7b8da7b93893
junjiewu1995 <42704445+junjiewu1995@users.noreply.github.com>
2021-03-27 16:23:57 -0700

    Update GoFishSever.go

--- server/GoFishSever.go
+++ server/GoFishSever.go
@@ -33,28 +33,66 @@ type Player struct {
 
 type GoFishServer struct {
 
-	Mu 				sync.Mutex
-	PlayerCounter 	int
-	dead            bool
-	Players         []Player
-	Deck			[]Card
-	turnIndex       int
-	TotalPlayers    int
+	Mu 				      sync.Mutex
+	PlayerCounter 	      int
+	dead                  bool
+	Players               []Player
+	Deck			      []Card
+	PlayerTurnIndex       int
+	TotalPlayers          int
 }
 
 
 func (gfs *GoFishServer)LoadConfiguration (file string) (GameConfig, error) {
+
     var config GameConfig
     configFile, err := os.Open(file)
+
     /* defer to the end and close the config file*/
     defer configFile.Close()
     if err != nil {
         return config, err
     }
+
     jsonParser := json.NewDecoder(configFile)
     err = jsonParser.Decode(&config)
     return config, err
 }
+
+
+/**
+ *  Player Set Up
+*/
+
+func (gfs *GoFishServer) PlayersSetUp (cardNum int) {
+    fmt.Println("2 players setting up begin ...")
+
+    /* Assign 7 Cards for Each Players */
+    for range gfs.Players {
+        for i := 0; i < cardNum; i++ {
+            gfs.drawCards(i)
+        }
+        /* Increase the TurnIndex Number */
+        gfs.PlayerTurnIndex += 1
+    }
+    gfs.PlayerTurnIndex = 0
+}
+
+
+
+func (gfs *GoFishServer) drawCards(i int) error {
+
+     /* Draw one card from the Deck and renew the Deck */
+     card := gfs.Deck[0]
+     gfs.Deck = gfs.Deck[1:]
+     card.Used = true
+     /* Give the Card to the assignPlayer */
+     gfs.Players[gfs.PlayerTurnIndex].Hand = append(gfs.Players[gfs.PlayerTurnIndex].Hand, card)
+
+     return nil
+}
+
+
 
 func (gfs *GoFishServer) EnterGame (playerask *CardRequest, reply *CardRequestReply) error {
 
@@ -62,32 +100,23 @@ func (gfs *GoFishServer) EnterGame (playerask *CardRequest, reply *CardRequestRe
     gfs.Mu.Lock()
     defer gfs.Mu.Unlock()
 
-    var p Player
-
     /* No More than 7 players */
     if gfs.PlayerCounter < 7 {
-
-        /* assign player info */
-        p.ID = gfs.PlayerCounter
-
         /* Add up the Player number */
         reply.ID = gfs.PlayerCounter
 
         /* Appends the players */
-        gfs.Players = append(gfs.Players, p)
+        gfs.Players = append(gfs.Players, Player{ID: gfs.PlayerCounter})
 
         /* add up the player counter */
         gfs.PlayerCounter += 1
     }
 
-    /* Once the Player meet the decides number Game starts */
-    if gfs.PlayerCounter == gfs.TotalPlayers {
-        fmt.Println("Game Starts ... ")
-        gfs.gameStart()
-    }
-
+    /* Once the Player meets the decides number Game starts */
+    if gfs.PlayerCounter == gfs.TotalPlayers { gfs.gameStart() }
     return nil
 }
+
 
 /*
  * gameStart => LoadCards()
@@ -95,32 +124,61 @@ func (gfs *GoFishServer) EnterGame (playerask *CardRequest, reply *CardRequestRe
 */
 
 func (gfs *GoFishServer) gameStart () {
+    fmt.Println("Game Initializes Environments ... ")
+    /* Load Cards */
     gfs.LoadCard()
+    /* Assign Initialized Cards */
     gfs.assignCard()
 }
 
+/**
+ * game initilization
+*/
+
 func (gfs *GoFishServer) assignCard () error {
 
-    for idx, _ := range gfs.Players {
-        for x, _ := range gfs.Deck {
-           var num = 0
-           if num < 5 {
-              gfs.Players[idx].Hand = append(gfs.Players[idx].Hand, CardValue)
-              num = num + 1
-           }
-        }
+    /* Check the Player Number */
+    switch {
+        case gfs.PlayerCounter == 1:
+            gfs.dead = true
+        case gfs.PlayerCounter == 2:
+            /* 2 players assign 7 cards */
+            gfs.PlayersSetUp(7)
+        default:
+            /* More than 2 players assign 5 cards */
+            gfs.PlayersSetUp(5)
     }
     return nil
 }
+
+/**
+ * The gameOver function would check the Status of the game
+*/
 
 func (gfs *GoFishServer) GetStatusOfGame () {
 
 }
 
+
+/**
+ * If the player does not have the cards, it would return One Card for the Player
+ */
+func (gfs *GoFishServer) goFish () {
+
+
+}
+
+
+/*
+ * Once the Player call RequestForCard for a particular player
+ * The server request the tartget Players' information
+ * And return back to the Caller or Call Go Fish
+*/
 func (gfs *GoFishServer) RequestForCard(ask *CardRequest, reply *CardRequestReply) error {
 
 	gfs.Mu.Lock()
 	defer gfs.Mu.Unlock()
+
 
 	reply.GoFishGame = false
 	reply.Turn = 1
@@ -157,15 +215,6 @@ func (gfs *GoFishServer) LoadCard() error {
     return nil
 }
 
-//Draw a card from the deck (returns card type)
-func (gfs *GoFishServer) goFish() {
-    
-    //draw a card from the deck, removing it from gfs.Deck
-    drawnCard = gfs.Deck[0]
-    gfs.Deck = gfs.Deck[1:]
-    
-    return drawnCard
-}
 
 /**
  * RPC server interaction
@@ -188,7 +237,10 @@ func (gfs *GoFishServer)serverStateSet() *GoFishServer {
 	return gfs
 }
 
-
+func (gfs *GoFishServer)gameOver() bool {
+    if gfs.dead == false { return false}
+    else {  return true }
+}
 
 /* Create a Game Server */
 func StartServer () *GoFishServer {
@@ -197,9 +249,13 @@ func StartServer () *GoFishServer {
 	gfs := GoFishServer{}
 	gfs.Deck = []Card{}
 
+    /* Intialize the Game Variables */
+
     gfs.TotalPlayers = 0
 	gfs.PlayerCounter = 0
+	gfs.PlayerTurnIndex = 0
 
+    /* Construct the Game Config file */
 	config, _ := gfs.LoadConfiguration("../game.config.json")
 
 	i1, err := strconv.Atoi(config.Gameset.HostPlayers)
@@ -209,9 +265,12 @@ func StartServer () *GoFishServer {
 	gfs.server()
     rep := gfs.serverStateSet()
 
-    /* Checking the game is over or not */
-    for gfs.dead == false {
-
+    /* Checking the game is over or not periodly */
+    for !gfs.gameOver() {
+        fmt.Println("Check the Game Status in 1 sec ...")
+        /* Check the game status */
+        gfs.GetStatusOfGame()
+        time.Sleep( 1 * time.Second)
     }
 
 	return rep
